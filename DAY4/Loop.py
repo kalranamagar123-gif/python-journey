fc70fdcd50b4f64c57c2a9852448d79f8d276dc5
kalranamagar123-gif <kalranamagar123@gmail.com>
2025-12-27 22:42:17 -0500

    Add loop examples in Loop.py

---
+++ DAY4/Loop.py
@@ -0,0 +1,16 @@
+for t in range(1, 11, 2):
+    print(t)
+
+for i in range(1, 20, 2):
+    print(i)
+
+for i in range(0, 101, 10):
+    print(i)
+
+for i in range(10, 0, -1):
+    print(i)
+
+
+for i in range(4, 5):
+    print(i)
+
