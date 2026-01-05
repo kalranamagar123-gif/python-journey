e390778cf4a982571ad3da71e08b3f3fe7969c37
kalranamagar123-gif <kalranamagar123@gmail.com>
2026-01-05 00:16:29 -0500

    Update student list operations and fix print syntax
    Fix syntax errors in print statements and add comments.

--- DAY8/day8_lists.py
+++ DAY8/day8_lists.py
@@ -1,5 +1,18 @@
+# Day8 - python lists
 students = ["Kalpana", "Kairabi", "Rodha", "Shreya", "Swarnima"]
+
+#Add a students
 students.append("Ashmita")
+
+#Remove a students
 students.remove("Swarnima")
+
+#Print all students
 for name in students:
     print(name)
+
+
+#Exatra practice
+print("Total students": len(students))
+print("First Students": students[0])
+print("Last Students": students[-1])
